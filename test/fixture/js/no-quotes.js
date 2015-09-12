a9ed42e8866d52a8a38a080bc3d2815f31ec6dc8
Sebastien <sebastien.saigo@gmail.com>
2015-09-12 19:56:34 +0900

    Added tests where value for the 'name' and 'filename' keys have no quotes in Content-Disposition parameters

---
+++ test/fixture/js/no-quotes.js
@@ -0,0 +1,13 @@
+var reference = {
+  type: 'file',
+  name: 'upload',
+  filename: 'another_plain.txt',
+  fixture: 'another_plain.txt',
+  sha1: '55af4ecf24e3ee887d19d843e8367eadbf3a6436'
+};
+
+module.exports = {
+  'filename-no-quotes.http': [ reference ],
+  'name-filename-no-quotes.http': [ reference ],
+  'name-no-quotes.http': [ reference ]
+};
