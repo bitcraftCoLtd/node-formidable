bf363d5bef52eaeef1e4853b4011851c865fc99b
Sebastien <sebastien.saigo@gmail.com>
2015-09-12 19:56:10 +0900

    Added test with empty value for the 'name' key in Content-Disposition parameters

--- test/fixture/js/no-filename.js
+++ test/fixture/js/no-filename.js
@@ -7,3 +7,8 @@ module.exports['filename-name.http'] = [
   {type: 'file', name: 'upload', filename: 'plain.txt', fixture: 'plain.txt',
   sha1: 'b31d07bac24ac32734de88b3687dddb10e976872'},
 ];
+
+module.exports['empty-name.http'] = [
+  {type: 'file', name: '', filename: 'plain.txt', fixture: 'plain.txt',
+  sha1: 'b31d07bac24ac32734de88b3687dddb10e976872'},
+];
